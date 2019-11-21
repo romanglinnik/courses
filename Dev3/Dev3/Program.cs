c3e92cdc482d983b521b60b5fef50c255c644a09
Roman Glinnik <roma.glinnik@gmail.com>
2019-11-21 16:46:35 +0300

    whithout tests

---
+++ Dev3/Dev3/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Dev3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string myString;
+            myString = Console.ReadLine();
+            TransliteManager str = new TransliteManager(myString);
+            Console.WriteLine(str.Translit(myString));
+        }
+    }
+}
