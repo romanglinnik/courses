832d0c7d2aeaad6ee02755e6ec305bca65f02a93
Roman Glinnik <roma.glinnik@gmail.com>
2019-11-21 16:41:42 +0300

    dev 2

---
+++ Dev2/Dev2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Dev2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string myString;
+            myString = Console.ReadLine();
+            String str = new String(myString);
+            Console.WriteLine(str.GetMaximumNumberOfUnequalConsecutiveCharactersInAString(myString));
+            Console.WriteLine(str.GetMaximumNumberOfIdenticalConsecutiveDigitsInAString(myString));
+            Console.WriteLine(str.GetMaximumNumberOfIdenticalConsecutiveLatinCharactersInAString(myString));
+        }
+    }
+}
