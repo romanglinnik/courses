bdc6b79819172a7d12bcd29a92d3678994c0ca28
Roman Glinnik <roma.glinnik@gmail.com>
2020-02-25 18:07:47 +0300

    Calculator Project Done

---
+++ CalculatorProject/CalculatorProject/Invoker.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+
+namespace CalkulyatorProj
+{
+    public class Invoker
+    {
+
+        CalkulyatorPage calkulyator { get; set; }
+
+
+        public Invoker(CalkulyatorPage calkulyator)
+        {
+            this.calkulyator = calkulyator;
+
+
+        }
+
+
+        public string GetResult(string str)
+        {
+            string[] ButtonsNames = str.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (var ButtonsName in ButtonsNames)
+            {
+                CalkulyatorPage.buttons[ButtonsName].Click();
+
+            }
+            return CalkulyatorPage.buttons["display"].Text;
+
+
+        }
+    }
+}
